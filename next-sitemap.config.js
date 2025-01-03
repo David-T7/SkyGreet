539ce26aeb3e9f2814cf964df46448d87df3f5d4
David_T7 <tesfayedawit524@gmail.com>
2025-01-03 09:05:33 -0300

    generated site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.skygreetservice.com/', // Replace with your site's URL
+    generateRobotsTxt: true, // Generates a robots.txt file
+    sitemapSize: 7000, // Optional, splits sitemap if exceeding size
+  };
+  
